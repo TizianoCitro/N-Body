b789a28b06c5aec7d592ae53b58a2eae0c01ede4
Tiziano <tizianocitro98@gmail.com>
2021-08-11 18:46:28 +0200

    Minor refactoring

--- nBody.c
+++ nBody.c
@@ -40,7 +40,7 @@ void printBodies(Body *bodies, int numberOfBodies, int numberOfTasks, int iterat
 void printHowToUse();
 
 int main(int argc, char **argv) {
-
+    
     // Initializes the MPI environment
     MPI_Init(NULL, NULL);
 
@@ -52,22 +52,28 @@ int main(int argc, char **argv) {
     // Gets the rank of the process
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
     
-    /**
-     * Handles input parameters.
-     * Stops the execution if they're not provided properly.
-     */
+    // Handles input parameters.
+    // Stops the execution if they're not provided properly.
     int isCorrectlyInvoked = CORRECTLY_INVOKED;
-    if (argc != EXPECTED_ARGUMENT) isCorrectlyInvoked = NOT_CORRECTLY_INVOKED;
+    if (argc != EXPECTED_ARGUMENT) {
+        isCorrectlyInvoked = NOT_CORRECTLY_INVOKED;
+    }
 
     int isPrintRequired;
     if (isCorrectlyInvoked != NOT_CORRECTLY_INVOKED) {  
-        if (strcmp(argv[PRINT_ARGUMENT], "-pY") == 0) isPrintRequired = PRINT_REQUIRED;
-        else if (strcmp(argv[PRINT_ARGUMENT], "-pN") == 0) isPrintRequired = PRINT_NOT_REQUIRED;
-        else isCorrectlyInvoked = NOT_CORRECTLY_INVOKED;
+        if (strcmp(argv[PRINT_ARGUMENT], "-pY") == 0) {
+            isPrintRequired = PRINT_REQUIRED;
+        } else if (strcmp(argv[PRINT_ARGUMENT], "-pN") == 0) {
+            isPrintRequired = PRINT_NOT_REQUIRED;
+        } else {
+            isCorrectlyInvoked = NOT_CORRECTLY_INVOKED;
+        }
     }
 
     if (isCorrectlyInvoked == NOT_CORRECTLY_INVOKED) {
-        if (rank == MASTER) printHowToUse();
+        if (rank == MASTER) {
+            printHowToUse();
+        }
 
         // Finalizes the MPI environment in case of error
         MPI_Finalize();
@@ -86,7 +92,8 @@ int main(int argc, char **argv) {
     if (rank == MASTER) {
         randomizeBodies(buffer, BODY_FLOAT * numberOfBodies);
         
-        printTimeAndBodies(bodies, numberOfBodies, numberOfTasks, iterations, NO_EXECUTION_TIME, EXECUTION_TIME_NOT_REQUIRED, isPrintRequired);
+        printTimeAndBodies(bodies, numberOfBodies, numberOfTasks, iterations, 
+            NO_EXECUTION_TIME, EXECUTION_TIME_NOT_REQUIRED, isPrintRequired);
     }
 
     // Creates the custom MPI datatype for bodies
@@ -94,20 +101,16 @@ int main(int argc, char **argv) {
     MPI_Datatype oldTypes[1] = {MPI_FLOAT};
     int blocksCount[1] = {BODY_FLOAT};
     MPI_Aint offset[1] = {0};
-    
     MPI_Type_create_struct(1, blocksCount, offset, oldTypes, &MPI_BODY);
     MPI_Type_commit(&MPI_BODY);
-
-    /**
-     * Contains the number of bodies to send to each of the processes
-     * and the displacements where each segment begins respectively.
-     * So, displs is about where to start and bodiesPerProcess is
-     * about how far you have to go.
-     */
+    
+    // Contains the number of bodies to send to each of the processes
+    // and the displacements where each segment begins respectively.
+    // So, displs is about where to start and bodiesPerProcess is
+    // about how far you have to go.
     int *bodiesPerProcess = (int*) malloc(numberOfTasks * sizeof(int));
     int *displs = (int*) malloc(numberOfTasks * sizeof(int));
     buildBodiesPerProcessAndDispls(numberOfBodies, numberOfTasks, bodiesPerProcess, displs);
-
     MPI_Request request = MPI_REQUEST_NULL;
     MPI_Request requests[numberOfTasks];
     MPI_Status status;
@@ -116,22 +119,16 @@ int main(int argc, char **argv) {
     int startTime = MPI_Wtime();
 
     // Master sends a portion of the bodies to each slave
-    MPI_Scatterv(
-        bodies, 
-        bodiesPerProcess, 
-        displs, 
-        MPI_BODY,
-        &bodies[displs[rank]], 
-        bodiesPerProcess[rank], 
-        MPI_BODY, 
-        MASTER,
-        MPI_COMM_WORLD);
+    MPI_Scatterv(bodies, bodiesPerProcess, displs, MPI_BODY, &bodies[displs[rank]], 
+        bodiesPerProcess[rank], MPI_BODY, MASTER, MPI_COMM_WORLD);
 
     // Computes for the number of requested iterations
     for (int iteration = 0; iteration < iterations; iteration++) {
-        for (int process = MASTER; process < numberOfTasks; process++)
-            MPI_Ibcast(&bodies[displs[process]], bodiesPerProcess[process], MPI_BODY, process, MPI_COMM_WORLD, &requests[process]);
-        
+        for (int process = MASTER; process < numberOfTasks; process++) {
+            MPI_Ibcast(&bodies[displs[process]], bodiesPerProcess[process], 
+                MPI_BODY, process, MPI_COMM_WORLD, &requests[process]);
+        }
+
         // Each of the process computes its own part of the bodies
         int independentStop = displs[rank] + bodiesPerProcess[rank];
         bodyForce(bodies, DT, displs[rank], independentStop, displs[rank], independentStop);
@@ -141,10 +138,8 @@ int main(int argc, char **argv) {
                 // Waits for the process with the same rank as waitedProcess
                 MPI_Wait(&requests[waitedProcess], &status);
                 
-                /**
-                 * Computes on its own particles compared with the particles 
-                 * of the process with the same rank as waitedProcess.
-                 */
+                // Computes on its own particles compared with the particles 
+                // of the process with the same rank as waitedProcess.
                 int dependentStop = displs[waitedProcess] + bodiesPerProcess[waitedProcess];
                 bodyForce(bodies, DT, displs[waitedProcess], dependentStop, displs[rank], independentStop);
             }
@@ -155,23 +150,16 @@ int main(int argc, char **argv) {
     }  
 
     // Gathers all the computation from slaves to master
-    MPI_Gatherv(
-        &bodies[displs[rank]], 
-        bodiesPerProcess[rank], 
-        MPI_BODY, 
-        bodies, 
-        bodiesPerProcess, 
-        displs, 
-        MPI_BODY, 
-        MASTER, 
-        MPI_COMM_WORLD);
+    MPI_Gatherv(&bodies[displs[rank]], bodiesPerProcess[rank], MPI_BODY, 
+        bodies, bodiesPerProcess, displs, MPI_BODY, MASTER, MPI_COMM_WORLD);
 
     MPI_Barrier(MPI_COMM_WORLD);
     int finishTime = MPI_Wtime();
-
     int executionTime = finishTime - startTime;
-    if (rank == MASTER) 
-        printTimeAndBodies(bodies, numberOfBodies, numberOfTasks, iterations, executionTime, EXECUTION_TIME_REQUIRED, isPrintRequired);
+    if (rank == MASTER) {
+        printTimeAndBodies(bodies, numberOfBodies, numberOfTasks, iterations, 
+            executionTime, EXECUTION_TIME_REQUIRED, isPrintRequired);
+    }
 
     free(bodiesPerProcess);
     free(displs);
@@ -184,11 +172,13 @@ int main(int argc, char **argv) {
 }
 
 void randomizeBodies(float *bodies, int numberOfBodies) {
-    for (int body = 0; body < numberOfBodies; body++) 
+    for (int body = 0; body < numberOfBodies; body++) {
         bodies[body] = 2.0f * (rand() / (float) RAND_MAX) - 1.0f;
+    }
 }
 
-void bodyForce(Body *bodies, float dt, int dependentStart, int dependentStop, int independentStart, int independentStop) {
+void bodyForce(Body *bodies, float dt, int dependentStart, int dependentStop, 
+        int independentStart, int independentStop) {
     for (int i = independentStart; i < independentStop; i++) {
         float Fx = 0.0f;
         float Fy = 0.0f;
@@ -221,45 +211,39 @@ void updatePositions(Body *bodies, float dt, int start, int stop) {
     }
 }
 
-void buildBodiesPerProcessAndDispls(int numberOfBodies, int numberOfTasks, int *bodiesPerProcess, int *displs) {
+void buildBodiesPerProcessAndDispls(int numberOfBodies, int numberOfTasks, 
+        int *bodiesPerProcess, int *displs) {
     int rest = numberOfBodies % numberOfTasks;
     int bodiesDifference = numberOfBodies / numberOfTasks;
     int startPosition = 0;
 
-    /**
-     * It's based on the fact that the rest
-     * is always less than the divisor.
-     */
+    // It's based on the fact that the rest is always less than the divisor
     for (int process = MASTER; process < numberOfTasks; process++) {
         if (rest > BODY_NO_DIFFERENCE) {
             bodiesPerProcess[process] = bodiesDifference + 1;
             rest--;
-        } else bodiesPerProcess[process] = bodiesDifference;
+        } else {
+            bodiesPerProcess[process] = bodiesDifference;
+        }
 
         displs[process] = startPosition;
         startPosition += bodiesPerProcess[process];
     }
 }
 
-void printTimeAndBodies(
-    Body *bodies, 
-    int numberOfBodies, 
-    int numberOfTasks,
-    int iterations,
-    double executionTime, 
-    int isExecutionTimeRequired, 
-    int isPrintRequired) {
+void printTimeAndBodies(Body *bodies, int numberOfBodies, int numberOfTasks, int iterations, 
+        double executionTime, int isExecutionTimeRequired, int isPrintRequired) {
     // If execution time is required then it's the end of computation
-    if (isPrintRequired == 1) printBodies(bodies, numberOfBodies, numberOfTasks, iterations, isExecutionTimeRequired);
+    if (isPrintRequired == 1) {
+        printBodies(bodies, numberOfBodies, numberOfTasks, iterations, isExecutionTimeRequired);
+    }
 
     if (isExecutionTimeRequired == 1) {
-        printf(
-            "With %d processors, %d bodies and %d iterations the execution time is %0.2f seconds\n", 
+        printf("With %d processors, %d bodies and %d iterations the execution time is %0.2f seconds\n", 
             numberOfTasks, numberOfBodies, iterations, executionTime);
 
         FILE *file = fopen("./nBodyExecutionTime.txt", "a");
-        fprintf(
-            file, 
+        fprintf(file, 
             "With %d processors, %d bodies and %d iterations the execution time is %0.2f seconds\n\n", 
             numberOfTasks, numberOfBodies, iterations, executionTime);
     }
@@ -268,16 +252,19 @@ void printTimeAndBodies(
 void printBodies(Body *bodies, int numberOfBodies, int numberOfTasks, int iterations, int isEnd) {
     FILE *file = fopen("./bodies.txt", "a");
 
-    if (isEnd == 1) fprintf(file, "Bodies at the end with %d processors and %d iterations:\n", numberOfTasks, iterations);
-    else fprintf(file, "Bodies at the beginning with %d processors and %d iterations:\n", numberOfTasks, iterations);
+    if (isEnd == 1) {
+        fprintf(file, "Bodies at the end with %d processors and %d iterations:\n", 
+            numberOfTasks, iterations);
+    } else {
+        fprintf(file, "Bodies at the beginning with %d processors and %d iterations:\n", 
+            numberOfTasks, iterations);
+    }
 
-    for(int body = 0; body < numberOfBodies; body++)
-		fprintf(
-            file, 
-            "Body[%d][%f, %f, %f, %f, %f, %f]\n", 
-            body,
+    for (int body = 0; body < numberOfBodies; body++) {
+		fprintf(file, "Body[%d][%f, %f, %f, %f, %f, %f]\n", body,
 			bodies[body].x, bodies[body].y, bodies[body].z,
 			bodies[body].vx, bodies[body].vy, bodies[body].vz);
+    }
 
     fprintf(file, "\n");
 }
